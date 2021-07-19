a781f584adebc6ad00a7926a9cf0e828111d1ed9
Marcin M <mmaczka@inpost.pl>
2021-07-19 23:23:26 +0200

    usuniecie pliku

--- src/MyJavaStart.java
+++
@@ -1,8 +0,0 @@
-public class MyJavaStart {
-    public static void main(String[] args) {
-        System.out.println("Cześć Marcin");
-        System.out.println("Gratulacje zmiany Teamu!");
-        System.out.println("Ale musisz przyłożyć się do nauki JAVA.");
-        System.out.println("Więc do roboty!");
-    }
-}
